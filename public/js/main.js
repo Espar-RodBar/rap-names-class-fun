e3b4748a0434a45f57b13d4c63b05f4cad666b37
Espar-RodBar <cr4wl3r@gmail.com>
2023-06-16 19:42:40 +0200

    fixed input main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ document.querySelector("button").addEventListener("click", getRapperName);
 const inputText = document.querySelector("input");
 
 async function getRapperName() {
-    const rapperName = inputText.textContent;
+    const rapperName = inputText.value;
     try {
         const res = await fetch(
             `https://good-rose-barracuda-tutu.cyclic.app//api/${rapperName}`
